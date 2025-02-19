b44b5898ef351a32c4b9b3ab75b0a2d652d60a89
Jean Huaringa <jeanhuaringach@gmail.com>
2025-02-19 01:40:42 -0500

    git init --> Proyecto base

---
+++ MyProjectAPI_Travel/Data/MyProjectTravelContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyProjectAPI_Travel.Data;
+
+public partial class MyProjectTravelContext : DbContext
+{
+    public MyProjectTravelContext()
+    {
+    }
+
+    public MyProjectTravelContext(DbContextOptions<MyProjectTravelContext> options)
+    : base(options)
+    {
+    }
+
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
+
